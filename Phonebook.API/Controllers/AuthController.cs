26c179c26192aa860783d45f757277b8786231cd
DESKTOP-QJSFM2M\Arjac <gareth.seaward@gmail.com>
2020-06-27 22:46:14 +0200

    Add Authentication structure on API

---
+++ Phonebook.API/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Phonebook.API.Data;
+using Phonebook.API.Dtos;
+using Phonebook.API.Models;
+using Phonebook.API.Utils;
+
+namespace Phonebook.API.Controllers
+{
+  [Route("v1/[controller]")]
+  [ApiController]
+  public class AuthController : ControllerBase
+  {
+    private readonly IAuthRepository _authRepo;
+    private readonly IJwtTokenGenerator _tokenGenerator;
+
+    public AuthController(IAuthRepository authRepo, IJwtTokenGenerator tokenGenerator)
+    {
+      _authRepo = authRepo;
+      _tokenGenerator = tokenGenerator;
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+    {
+      userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+      if (await _authRepo.DoesUserExist(userForRegisterDto.Username))
+        return BadRequest("username already exists");
+
+      var userToCreate = new User
+      {
+        Username = userForRegisterDto.Username
+      };
+
+      var createdUser = await _authRepo.Register(userToCreate, userForRegisterDto.Password);
+
+      return StatusCode(201);
+    }
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+    {
+      var userFormrepo = await _authRepo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+      if (userFormrepo is null) return Unauthorized();
+
+      var jwtToken = _tokenGenerator.GetJwt(userFormrepo);
+
+      return Ok(new { token = jwtToken });
+    }
+  }
+}
---
+++ Phonebook.API/Controllers/PhonebooksControler.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Phonebook.API.Data;
+
+namespace DatingApp.API.Controllers
+{
+
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class PhoneBooksController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PhoneBooksController(DataContext context)
+        {
+            _context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetPhoneBooks()
+        {
+            var phonebooks = await _context.Phonebooks.ToListAsync();
+
+            return Ok(phonebooks);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPhonebook(int id)
+        {
+            var phonebook = await _context.Phonebooks.FindAsync(id);
+
+            return Ok(phonebook); //204 if no content
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
